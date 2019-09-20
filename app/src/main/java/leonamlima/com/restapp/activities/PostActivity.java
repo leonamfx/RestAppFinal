d9abb9448f781b0aff6f1e7b59a6c9eadcc69ee1
Leonam Lima <leonamfx@gmail.com>
2019-09-20 19:18:48 -0300

    Final

--- app/src/main/java/leonamlima/com/restapp/activities/PostActivity.java
+++ app/src/main/java/leonamlima/com/restapp/activities/PostActivity.java
@@ -3,14 +3,45 @@ package leonamlima.com.restapp.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import leonamlima.com.restapp.R;
+import leonamlima.com.restapp.entidades.Post;
+import leonamlima.com.restapp.services.RetrofitService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class PostActivity extends AppCompatActivity {
 
+    private static final String TAG = "PostActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+        enviarCosnultaRest();
+    }
+
+    private void enviarCosnultaRest() {
+        Post post = new Post(1,"Teste","Loren Ipsum");
+        RetrofitService.getServico().criarPost(post).enqueue(new Callback<Post>() {
+            @Override
+            public void onResponse(Call<Post> call, Response<Post> response) {
+                int cod = response.code();
+                int id = response.body().getId();
+                TextView textViewid = findViewById(R.id.textViewNovoIdCriado);
+                TextView textViewcod = findViewById(R.id.textViewCodigoDeRetorno);
+
+            textViewcod.setText(""+cod);
+            textViewid.setText(""+id);
+
+            }
+
+            @Override
+            public void onFailure(Call<Post> call, Throwable t) {
+                Log.d(TAG, "onFailure: "+t.getMessage());
+            }
+        });
     }
 }
--- app/src/main/java/leonamlima/com/restapp/services/ApiEndPoint.java
+++ app/src/main/java/leonamlima/com/restapp/services/ApiEndPoint.java
@@ -2,11 +2,17 @@ package leonamlima.com.restapp.services;
 
 import java.util.List;
 
+import leonamlima.com.restapp.entidades.Post;
 import leonamlima.com.restapp.entidades.User;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface ApiEndPoint {
     @GET("users")
     Call<List<User>> obterUsuarios();
+
+    @POST("posts")
+    Call<Post> criarPost(@Body Post post);
 }
