5a72a268d81fa50c2d17f6c9a2110c27f9b6cd9d
Leonam Lima <leonamfx@gmail.com>
2019-09-20 10:33:47 -0300

    Criação das classes de entidade

--- app/src/main/java/leonamlima/com/restapp/activities/MainActivity.java
+++ app/src/main/java/leonamlima/com/restapp/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package leonamlima.com.restapp;
+package leonamlima.com.restapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import leonamlima.com.restapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
