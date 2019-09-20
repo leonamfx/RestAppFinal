b94010993e1a0b8ad8f7d9adc8ee239691b86910
Leonam Lima <leonamfx@gmail.com>
2019-09-20 12:34:54 -0300

    Criacao Class Post

---
+++ app/src/main/java/leonamlima/com/restapp/activities/PostActivity.java
@@ -0,0 +1,16 @@
+package leonamlima.com.restapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import leonamlima.com.restapp.R;
+
+public class PostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+    }
+}
